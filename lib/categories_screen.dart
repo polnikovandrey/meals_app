b84680f832b6fc52b4d5a981e745884734b25666
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-06-11 16:04:07 +0300

    +- CategoriesScreen as home widget + CategoryItem + builder

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        maxCrossAxisExtent: 200,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
       ),
-      itemBuilder: (context, index) => const Text(''),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          maxCrossAxisExtent: 200,
+        ),
+        itemCount: dummyCategories.length,
+        itemBuilder: (context, index) {
+          return CategoryItem(dummyCategories[index]);
+        },
+      ),
     );
   }
 }
---
+++ lib/category_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String _title;
+  final Color _color;
+
+  CategoryItem(Category category, {Key? key})
+      : _title = category.title,
+        _color = category.color,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            _color.withOpacity(0.7),
+            _color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Text(_title),
+    );
+  }
+}
