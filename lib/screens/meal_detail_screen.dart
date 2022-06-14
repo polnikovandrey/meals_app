64e48904467abf50cb8e7cd1f82c7b54ef554650
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-06-14 20:23:31 +0300

    + MealDetailScreen ...3

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -12,13 +12,78 @@ class MealDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context)?.settings.arguments as Map<String, Object>;
     final Meal meal = routeArgs[MealItem.mealArgument] as Meal;
+    var themeData = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(meal.id),
+        title: Text(meal.title),
       ),
-      body: Center(
-        child: Text(meal.title),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            _buildSectionTitle(themeData, 'Ingredients'),
+            _buildSectionListContainer(
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) => Card(
+                  color: themeData.colorScheme.secondary,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                ),
+              ),
+            ),
+            _buildSectionTitle(themeData, 'Steps'),
+            _buildSectionListContainer(
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (ctx, index) => Column(children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('# ${index + 1}'),
+                    ),
+                    title: Text(meal.steps[index]),
+                  ),
+                  const Divider(),
+                ]),
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildSectionTitle(ThemeData themeData, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: themeData.textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget _buildSectionListContainer(Widget sectionContent) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: 150,
+      width: 300,
+      child: sectionContent,
     );
   }
 }
